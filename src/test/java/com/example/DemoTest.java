da52bbc1eeb17c79e394576260db34b758ee8fdb
farmud <2415728481@qq.com>
2025-11-30 14:33:11 +0800

    first commit

---
+++ src/test/java/com/example/DemoTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.Test;
+
+public class DemoTest {
+
+    @Test
+    public void sampleTest() {
+        System.out.println("This is a sample test.");
+    }
+}
